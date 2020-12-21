09648c49ad4021b36dab5ee5e4d16c98bf32e4c1
cspahn <cspahn07@penguin>
2020-12-20 20:58:28 -0500

    changes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint =Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    constrainedlog=new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -36,6 +36,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    chain=new Chain(bird.body,constrainedlog.body)
 }
 
 function draw(){
@@ -61,4 +62,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedlog.display();
+    
+    chain.display();
 }
